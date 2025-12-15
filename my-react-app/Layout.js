f3c4b079fe9778f9eb7da448f4723db0777308f1
yuto-masuki-hue <yuto-masuki@funaisoken.co.jp>
2025-12-15 15:51:19 +0900

    Delete my-react-app directory

--- my-react-app/Layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Layout({ children }) {
-  return (
-    <div className="min-h-screen bg-gray-50 font-sans antialiased text-gray-900">
-      {children}
-    </div>
-  );
-}
